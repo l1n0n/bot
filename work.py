6f3296f9be199883e25d1312adb195a5e82a3ea4
l1n0n <linar.kasimov07@mail.ru>
2026-04-05 10:55:17 +0300

    a

--- work.py
+++ work.py
@@ -32,6 +32,6 @@ def send_schedule(message):
 
 @t.message_handler(commands=['date'])
 def send_date(message):
-    t.send_message(message.chat.id, str(weekdays))
+    t.send_message(message.chat.id, toString(datetime.now()))
 
 t.infinity_polling()
