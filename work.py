5642019ef0f984563d409f48fba3073e38e55b90
l1n0n <linar.kasimov07@mail.ru>
2026-04-17 17:41:23 +0300

    a

--- work.py
+++ work.py
@@ -221,7 +221,6 @@ def check_subscription(message):
 def getCorrectDate():
     old = (datetime.now() - datetime(1970, 1, 1, 0, 0, 0, 0)).total_seconds() + 10800
     new = datetime.fromtimestamp(old)
-    new = datetime(2026, 4, 18, 12, 34, 56)
     return new
 
 @t.message_handler(commands=['start'])
